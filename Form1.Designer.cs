0361142b82b6203bd4fffe8dd27a95aa886755f2
WamPan <markin.max.003@gmail.com>
2023-06-03 19:14:34 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DZ_USKOV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdlai = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxPers = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonAdlai
+            // 
+            this.buttonAdlai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdlai.Location = new System.Drawing.Point(154, 253);
+            this.buttonAdlai.Name = "buttonAdlai";
+            this.buttonAdlai.Size = new System.Drawing.Size(135, 72);
+            this.buttonAdlai.TabIndex = 0;
+            this.buttonAdlai.Text = "Расчет по адлаю";
+            this.buttonAdlai.UseVisualStyleBackColor = true;
+            this.buttonAdlai.Click += new System.EventHandler(this.buttonAdlai_Click_1);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(154, 40);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(154, 85);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 22);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // textBoxPers
+            // 
+            this.textBoxPers.Location = new System.Drawing.Point(154, 133);
+            this.textBoxPers.Name = "textBoxPers";
+            this.textBoxPers.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPers.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxPers);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.buttonAdlai);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdlai;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxPers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ_USKOV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Stopwatch stopwatch2 = new Stopwatch();
+
+        private void buttonAdlai_Click(object sender, EventArgs e)
+        {
+                
+        }
+
+        // Метод для вычисления периметра эллипса с использованием точной формулы Джеймса Айвори
+        static double CalculateEllipsePerimeterWithJamesIvory(double a, double b, int precision)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            double p = Math.PI * (3 * (a + b) - Math.Sqrt((3 * a + b) * (a + 3 * b)));
+            stopwatch.Stop();
+            return Math.Round(p, precision);
+            
+        }
+
+        // Метод для вычисления периметра эллипса с использованием формулы Фридриха Бесселя
+        static double CalculateEllipsePerimeterWithBessel(double a, double b, int precision)
+        {
+            Stopwatch stopwatch1 = new Stopwatch();
+            stopwatch1.Start();
+            double m = Math.Sqrt((a - b) * (a + b)) / (a + b);
+            double p = Math.PI * (a + b) * (1 + (3 * m * m) / (10 + Math.Sqrt(4 - 3 * m * m)));
+            stopwatch1.Stop();
+            return Math.Round(p, precision);
+        }
+
+        private void buttonAdlai_Click_1(object sender, EventArgs e)
+        {
+            // Ввод длин полуосей эллипса
+            Console.Write("Введите длину большой полуоси a: ");
+            double a = double.Parse(textBoxA.Text);
+
+            Console.Write("Введите длину малой полуоси b: ");
+            double b = double.Parse(textBoxB.Text);
+
+            // Ввод точности вычислений
+            Console.Write("Введите желаемую точность вычислений (количество знаков после запятой): ");
+            int precision = int.Parse(textBoxPers.Text);
+
+            // Рассчитываем периметр эллипса
+            double pIvory = CalculateEllipsePerimeterWithJamesIvory(a, b, precision);
+            double pBessel = CalculateEllipsePerimeterWithBessel(a, b, precision);
+
+            // Выводим результаты
+            Console.WriteLine($"Периметр эллипса с полуосями a = {a:0.###} и b = {b:0.###}:");
+            Console.WriteLine($"  с использованием точной формулы Джеймса Айвори: {pIvory}");
+            Console.WriteLine($"  с использованием формулы Фридриха Бесселя: {pBessel}");
+            //Console.WriteLine(stopwatch);
+        }
+    }
+}
